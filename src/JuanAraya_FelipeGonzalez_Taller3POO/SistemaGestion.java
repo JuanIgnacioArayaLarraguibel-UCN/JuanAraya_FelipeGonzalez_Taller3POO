a2f3deb7b4eae7887e65794b69ec1cd130f1ad8c
JuanIgnacioArayaLarraguibel-UCN <juan.araya02@alumnos.ucn.cl>
2025-11-21 15:41:43 -0300

    Update SistemaGestion.java

--- src/JuanAraya_FelipeGonzalez_Taller3POO/SistemaGestion.java
+++ src/JuanAraya_FelipeGonzalez_Taller3POO/SistemaGestion.java
@@ -1,16 +1,48 @@
 package JuanAraya_FelipeGonzalez_Taller3POO;
 
 import java.io.File;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
 public class SistemaGestion {
 	// supongo que aqui ira todo el desastre?
 
-	private List<Usuario> usuarios;
+	private static SistemaGestion instancia;
+    private List<Usuario> usuarios;
+    private List<Proyecto> proyectos;
+    private List<Tarea> todasLasTareas;
+    private Usuario usuarioActual;
+    private Scanner scanner;
 
+    private EstrategiaPrioridad estrategiaPrioridad;
+    
+    private SistemaGestion() {
+        usuarios = new ArrayList<>();
+        proyectos = new ArrayList<>();
+        todasLasTareas = new ArrayList<>();
+        scanner = new Scanner(System.in);
+        cargarDatos();
+    }
+    
+    public static SistemaGestion getInstancia() {
+        if (instancia == null) {
+            instancia = new SistemaGestion();
+        }
+        return instancia;
+    }
+    
+    /**
+     * cargar los datos para el ordenamiento y las preferencias
+     */
+    private void cargarDatos() {
+        cargarUsuarios();
+        cargarProyectos();
+        cargarTareas();
+    }
+    
 	// lectores
-	public void lectorUsuario() {
+	public void cargarUsuarios() {
 		try {
 			File archivo = new File("usuarios.txt");
 			Scanner lectorScanner = new Scanner(archivo);
